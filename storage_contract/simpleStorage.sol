8c1c896d8f3bb205565e9d2ab18128f63527eefa
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-24 17:47:54 +0100

    all data types

--- storage_contract/simpleStorage.sol
+++ storage_contract/simpleStorage.sol
@@ -3,5 +3,10 @@
 pragma solidity >=0.6.0 < 0.9.0;
 
 contract simpleStorage {
-    
+    uint256 favoriteNumber = 5;
+    bool favouriteBool = true;
+    string favoriteString = "Strings";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0xEbBe13f0F5bF9279f6b74EcBbA91AE06c0D714d7;
+    bytes32 favoriteBytes = "cat";
 }
