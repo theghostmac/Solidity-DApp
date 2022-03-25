7109c6c3d3f7561a66a982214a85944931203574
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-25 17:53:43 +0100

    storage contract

--- storage_contract/simpleStorage.sol
+++ storage_contract/simpleStorage.sol
@@ -3,7 +3,9 @@
 pragma solidity >=0.6.0 < 0.9.0;
 
 contract simpleStorage {
+    // a variable set to public
     uint256 public favoriteNumber = 5;
+    // a function that receives and store a number to public
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
